374adb1cc9642a611d6ee0790bebafb0402ef0ba
github username <github email address>
2019-10-17 16:57:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+class Animal{
+    constructor(name, sex){
+        this.name = name
+        this.sex = sex
+    }
+    speak() {
+        if(this instanceof Cat){
+            return this.name + ' says meow!'
+        }else if(this instanceof Dog){
+            return this.name + ' says woof!'
+        }else if(this instanceof Bird){
+            const maleResp =  "It's me! " + this.name + ", the parrot!"
+            const femaleResp = this.name + " says squawk!"
+            return this.sex === "male" ? maleResp : femaleResp
+        }
+    }
+}
+
+class Cat extends Animal{
+
+}
+
+class Dog extends Animal{
+
+}
+
+class Bird extends Animal{
+ 
+}
